6b47507fbfd8c1ed3131519a9217e20d70b6618d
Jonathan K <jkamagi41@gmail.com>
2026-01-02 16:14:39 +0800

    clean: penilaian

--- be/models/kriteria.py
+++ be/models/kriteria.py
@@ -4,8 +4,7 @@ class Kriteria(db.Model):
     __tablename__ = 'kriteria'
     id = db.Column(db.Integer, primary_key=True)
     kode = db.Column(db.String(5), unique=True, nullable=False) # C1, C2, dst [cite: 622]
-    nama = db.Column(db.String(100), nullable=False) # Harga, Kecepatan, dst [cite: 622]
-    # Sifat: 'benefit' atau 'cost' 
+    nama = db.Column(db.String(100), nullable=False)    
     sifat = db.Column(db.String(10), nullable=False) 
 
     def __repr__(self):
