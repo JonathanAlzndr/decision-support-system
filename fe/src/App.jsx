b0726ad4b521e65e213a6bf8d51f5aef17679a43
marcoismakalew <marcoismakalew@gmail.com>
2026-01-03 16:13:35 +0800

    feat login page just one page for admin & user

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -3,16 +3,18 @@ import "./index.css";
 import LandingPage from "./pages/LandingPage";
 import ProtectedRoute from "./Security/ProtectedRoute";
 import NotFoundPage from "./pages/NotFoundPage";
+import { ProtectedUserRoute } from "./Security/ProtectedRoute";
+import Login from "./pages/Login";
 
 /* admin pages */
 import AlternatifAdmin from "./pages/admin/AlternatifAdmin";
 import DetailPerhitungan from "./pages/admin/DetailPerhitungan";
 import KriteriaAdmin from "./pages/admin/KriteriaAdmin";
-import AdminLogin from "./pages/admin/AdminLogin";
+import LoginAdmin from "./pages/admin/LoginAdmin";
 import BerandaAdmin from "./pages/admin/BerandaAdmin";
 
 /* user pages */
-import UserLogin from "./pages/user/UserLogin";
+import LoginUser from "./pages/user/LoginUser";
 import DataMotorPage from "./pages/user/DataMotorPage";
 import HasilRekomendasiPage from "./pages/user/HasilRekomendasiPage";
 
@@ -28,7 +30,7 @@ function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<LandingPage />} />
-			<Route path="admin/login" element={<AdminLogin />} />
+			<Route path="admin/login" element={<Login portal="Admin" />} />
 			<Route
 				path="/admin"
 				element={
@@ -42,8 +44,15 @@ function App() {
 				<Route path="kriteria" element={<KriteriaAdmin />} />
 				<Route path="perhitungan" element={<DetailPerhitungan />} />
 			</Route>
-			<Route path="/user" element={<UserLogin />} />
-			<Route path="/user" element={<UserLayout />}>
+			<Route path="/user/login" element={<Login portal="User" />} />
+			<Route
+				path="/user"
+				element={
+					<ProtectedUserRoute>
+						<UserLayout />
+					</ProtectedUserRoute>
+				}
+			>
 				<Route path="data" element={<DataMotorPage />} />
 				<Route path="kriteria" element={<KriteriaPage />} />
 				<Route path="hasil" element={<HasilRekomendasiPage />} />
--- fe/src/Security/ProtectedRoute.jsx
+++ fe/src/Security/ProtectedRoute.jsx
@@ -3,11 +3,17 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
 	const role = localStorage.getItem("role");
-
 	if (role !== "Admin") {
 		return <Navigate to="/admin/login" replace />;
 	}
+	return children;
+};
 
+export const ProtectedUserRoute = ({ children }) => {
+	const role = localStorage.getItem("role");
+	if (role !== "User") {
+		return <Navigate to="/user/login" replace />;
+	}
 	return children;
 };
 
