fbbbb033850814946e694f4b1f8ae78bcc236d06
marcoismakalew <marcoismakalew@gmail.com>
2026-01-13 14:31:29 +0800

    fix: Login

--- fe/src/Security/ProtectedRoute.jsx
+++ fe/src/Security/ProtectedRoute.jsx
@@ -5,7 +5,6 @@ export const ProtectedAdminRoute = ({ children }) => {
 	const token = localStorage.getItem("token");
 	const role = localStorage.getItem("role");
 
-	// Jika tidak ada token ATAU role bukan Admin, lempar ke login admin
 	if (!token || role !== "Admin") {
 		return <Navigate to="/admin/login" replace />;
 	}
@@ -16,7 +15,6 @@ export const ProtectedUserRoute = ({ children }) => {
 	const token = localStorage.getItem("token");
 	const role = localStorage.getItem("role");
 
-	// Jika tidak ada token ATAU role bukan User, lempar ke login user
 	if (!token || role !== "user") {
 		return <Navigate to="/user/login" replace />;
 	}
